3e7c1b688659fc4e0d2d6dd50b359357f758295e
Tanushree Paul <125464289+tanu0604@users.noreply.github.com>
2025-04-25 22:21:52 +0530

    Create Home.jsx

---
+++ feedback/src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="d-flex vh-100 justify-content-center align-items-center">
+      <h1 className="text-primary text-center">Welcome to Feedback Page</h1>
+    </div>
+  );
+};
+
+export default Home;
